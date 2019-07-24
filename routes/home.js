5b72998a3d25475bfec0065de44b19751030fa44
bradenn <braden@bradencnicholson.com>
2019-07-24 08:54:51 -0700

    The big bang

---
+++ routes/home.js
@@ -0,0 +1,14 @@
+var router = require('express').Router();
+let User = require('../models/user');
+
+router.get('/', function(req, res, next) {
+  User.findById(req.session.userId, function(err, user){
+    return res.render("home", {
+      title: "Home",
+      user: user
+    });
+  });
+
+});
+
+module.exports = router;
---
+++ routes/login.js
@@ -0,0 +1,88 @@
+let router = require('express').Router();
+let User = require('../models/user');
+
+router.get('/', function(req, res, next) {
+  User.findById(req.session.userId, function(err, user) {
+    if (user != null) return res.redirect("/");
+    return res.render("login", {
+      title: "Login",
+      user: user
+    });
+  });
+});
+
+router.post('/', function(req, res, next) {
+  if (req.body.password !== req.body.confPassword) {
+    var err = new Error('Passwords do not match.');
+    err.status = 400;
+    res.send("passwords dont match");
+    return next(err);
+  }
+  if (req.body.email && req.body.user && req.body.firstname &&
+    req.body.password && req.body.lastname && req.body.confPassword) {
+    var userData = {
+      email: req.body.email,
+      username: req.body.user,
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      password: req.body.password
+    }
+    User.create(userData, function(error, user) {
+      if (error) {
+        return res.render("login", {
+          title: "Login",
+          user: user,
+          error: {
+            type: "register",
+            message: "This username or email is taken."
+          }
+        });
+      } else {
+        req.session.userId = user._id;
+
+        return res.redirect('/');
+      }
+    });
+  } else if (req.body.logUser && req.body.logPassword) {
+    User.authenticate(req.body.logUser, req.body.logPassword, function(error, user) {
+      if (error || !user) {
+        User.findById(req.session.userId)
+          .exec(function(error, user) {
+            if (error) {
+              return next(error);
+            } else {
+              return res.render("login", {
+                title: "Login",
+                user: user,
+                error: {
+                  type: "login",
+                  message: "Incorrect username or password"
+                }
+              });
+            }
+          });
+      } else {
+        req.session.userId = user._id;
+        return res.redirect('/');
+      }
+    });
+  } else {
+    User.findById(req.session.userId)
+      .exec(function(error, user) {
+        if (error) {
+          return next(error);
+        } else {
+          return res.render("login", {
+            title: "Login",
+            user: user,
+            error: {
+              type: "register",
+              message: "All fields must be complete"
+            }
+          });
+        }
+      });
+  }
+});
+
+module.exports = router;
