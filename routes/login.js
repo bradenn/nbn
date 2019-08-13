9dbd81882be8833a89287e0c1f5f5da6a3a48a3f
bradenn <braden@bradencnicholson.com>
2019-08-12 19:46:04 -0700

    Made a feature e that detects idiots and tell them about how stupid they are. Shame on you for putting spaces in your usernamesgit add .

--- routes/login.js
+++ routes/login.js
@@ -25,73 +25,87 @@ router.post('/', function(req, res, next) {
     res.send("passwords dont match");
     return next(err);
   }
+
+
   if (req.body.email && req.body.user && req.body.firstname &&
     req.body.password && req.body.lastname && req.body.confPassword) {
-    var userData = {
-      email: req.body.email,
-      username: req.body.user,
-      firstname: req.body.firstname,
-      lastname: req.body.lastname,
-      password: req.body.password,
-      picture: "https://bn-media-aws.s3.us-west-2.amazonaws.com/1564603161391",
-      account: "user",
-      date: new Date()
-    }
+    var userNameString = req.body.user;
+    if (userNameString.includes(" ") == false){
+      var userData = {
+        email: req.body.email,
+        username: req.body.user,
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        password: req.body.password,
+        picture: "https://bn-media-aws.s3.us-west-2.amazonaws.com/1564603161391",
+        account: "user",
+        date: new Date()
+      }
     User.create(userData, function(error, user) {
-        if (error) {
-          return res.render("login", {
-            title: "Login",
-            user: user,
-            error: {
-              type: "register",
-              message: "This username or email is taken."
-            }
-          });
-        } else {
-          req.session.userId = user._id;
-          return res.redirect('/');
-        }
-    });
-} else if (req.body.logUser && req.body.logPassword) {
-  User.authenticate(req.body.logUser, req.body.logPassword, function(error, user) {
-    if (error || !user) {
-      User.findById(req.session.userId)
-        .exec(function(error, user) {
-          if (error) {
-            return next(error);
-          } else {
-            return res.render("login", {
-              title: "Login",
-              user: user,
-              error: {
-                type: "login",
-                message: "Incorrect username or password"
-              }
-            });
-          }
-        });
-    } else {
-      req.session.userId = user._id;
-      return res.redirect('/');
-    }
-  });
-} else {
-  User.findById(req.session.userId)
-    .exec(function(error, user) {
       if (error) {
-        return next(error);
-      } else {
         return res.render("login", {
           title: "Login",
           user: user,
           error: {
             type: "register",
-            message: "All fields must be complete"
+            message: "This username or email is taken."
           }
         });
+      } else {
+        req.session.userId = user._id;
+        return res.redirect('/');
       }
     });
-}
+  }else{
+    return res.render("login", {
+      title: "Login",
+      user: null,
+      error: {
+        type: "register",
+        message: "You fucking bafoon! Who the fuck puts a god damn space in their fucking username!? Try again."
+      }
+    });
+  }
+  } else if (req.body.logUser && req.body.logPassword) {
+    User.authenticate(req.body.logUser, req.body.logPassword, function(error, user) {
+      if (error || !user) {
+        User.findById(req.session.userId)
+          .exec(function(error, user) {
+            if (error) {
+              return next(error);
+            } else {
+              return res.render("login", {
+                title: "Login",
+                user: user,
+                error: {
+                  type: "login",
+                  message: "Incorrect username or password"
+                }
+              });
+            }
+          });
+      } else {
+        req.session.userId = user._id;
+        return res.redirect('/');
+      }
+    });
+  } else {
+    User.findById(req.session.userId)
+      .exec(function(error, user) {
+        if (error) {
+          return next(error);
+        } else {
+          return res.render("login", {
+            title: "Login",
+            user: user,
+            error: {
+              type: "register",
+              message: "All fields must be complete"
+            }
+          });
+        }
+      });
+  }
 });
 
 module.exports = router;
