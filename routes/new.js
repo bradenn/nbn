8b8dba72ddfb8239e48faafb54bf22d3c81c1fb6
bradenn <braden@bradencnicholson.com>
2019-08-30 20:47:46 -0700

    Wow

--- routes/new.js
+++ routes/new.js
@@ -5,7 +5,7 @@ let Topic = require('../models/topic');
 router.get('/', function(req, res, next) {
   User.findById(req.session.userId, function(err, user) {
     if (user == null) return res.redirect("/login");
-    if(user.account == "user") return res.redirect("/login");
+    if (user.account == "user") return res.redirect("/login");
 
     return res.render("new", {
       title: "New",
@@ -19,10 +19,20 @@ const singleUpload = upload.single('image');
 
 router.post('/topic', upload.single('image'), function(req, res, next) {
 
-  var img = (typeof req.file == "undefined")?req.body.link:req.file.location;
+  var img = (typeof req.file == "undefined") ? req.body.link : req.file.location;
 
   var tags = req.body.tags;
   var tagArray = tags.split(" ");
+  var bannedWords = require("../banned-words.json");
+
+  var standing = 0;
+  let bodyText = req.body.body;
+
+  bannedWords.words.forEach(function(wrd) {
+    if (bodyText.includes(wrd)) {
+      standing += (standing >= 2) ? 0 : 1;
+    }
+  });
 
   var topicData = {
     title: req.body.title,
@@ -31,9 +41,10 @@ router.post('/topic', upload.single('image'), function(req, res, next) {
     picture: img,
     desc: req.body.desc,
     section: req.body.section,
+    standing: standing,
     tags: tagArray,
     views: 0,
-    nsfw: (!req.body.nsfw)?false:true,
+    nsfw: (!req.body.nsfw) ? false : true,
     date: new Date()
   }
 
